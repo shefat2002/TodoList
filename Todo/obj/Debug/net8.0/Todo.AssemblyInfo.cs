955633dd73dc6a346710991a5ab2d1c27d9a61f2
shefat2002 <shefat2002@gmail.com>
2024-07-07 00:18:26 +0600

    Created Form.cshtml, updated the layout view

--- Todo/obj/Debug/net8.0/Todo.AssemblyInfo.cs
+++ Todo/obj/Debug/net8.0/Todo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Todo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c10b67a664daa32a0d281e00c3f52089b40be051")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f713220d4e676223054f38f8dc93f585ca27222c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Todo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Todo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
